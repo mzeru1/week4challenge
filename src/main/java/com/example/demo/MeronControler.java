51e8b42dd2e6d0ef0e28281abed7da6147169d71
Meron Zeru <sologolom@gmail.com>
2019-05-10 11:08:58 -0400

    secon commit

--- src/main/java/com/example/demo/MeronControler.java
+++ src/main/java/com/example/demo/MeronControler.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MeronControler {
+    @RequestMapping("/")
+    public String home(){
+        return "bloghome";
+    }
 
 }
