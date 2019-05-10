d1d4f5e399c216971b1b48d978c204d73bee8651
ryuska <yromanova2010@gmail.com>
2019-05-10 11:23:19 -0400

    addedGithub page

--- src/main/java/com/example/demo/YuliaControler.java
+++ src/main/java/com/example/demo/YuliaControler.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class YuliaControler {
+    @RequestMapping("/gitHub")
+    public String gitHub(){
+        return "gitHub";
+    }
 }
