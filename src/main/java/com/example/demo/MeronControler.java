0487a2e28ab2bde51de7a9f9bd27823841ac0d82
ryuska <yromanova2010@gmail.com>
2019-05-10 14:34:35 -0400

    last

--- src/main/java/com/example/demo/MeronControler.java
+++ src/main/java/com/example/demo/MeronControler.java
@@ -9,5 +9,39 @@ public class MeronControler {
     public String home(){
         return "bloghome";
     }
+        @RequestMapping("/gitHub")
+        public String gitHub() {
 
+            return "gitHub";
+        }
+
+        @RequestMapping("/programDesign")
+        public String programDesign() {
+            return "programDesign";
+        }
+
+        @RequestMapping("/coreJava")
+        public String coreJava() {
+            return "coreJava";
+        }
+
+        @RequestMapping("/oop")
+        public String oop() {
+            return "oop";
+        }
+
+        @RequestMapping("/hcb")
+        public String hcb() {
+            return "hcb";
+        }
+
+        @RequestMapping("/jbc")
+        public String jbc() {
+            return "jbc";
+        }
+
+        @RequestMapping("gitHub1")
+        public String gitHub1() {
+            return "gitHub1";
+        }
 }
--- src/main/java/com/example/demo/YuliaControler.java
+++ src/main/java/com/example/demo/YuliaControler.java
@@ -1,43 +1,43 @@
-package com.example.demo;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class YuliaControler {
-    @RequestMapping("/gitHub")
-    public String gitHub() {
-
-        return "gitHub";
-    }
-
-    @RequestMapping("/programDesign")
-    public String programDesign() {
-        return "programDesign";
-    }
-
-    @RequestMapping("/coreJava")
-    public String coreJava() {
-        return "coreJava";
-    }
-
-    @RequestMapping("/oop")
-    public String oop() {
-        return "oop";
-    }
-
-    @RequestMapping("/hcb")
-    public String hcb() {
-        return "hcb";
-    }
-
-    @RequestMapping("/jbc")
-    public String jbc() {
-        return "jbc";
-    }
-
-    @RequestMapping("gitHub1")
-    public String gitHub1() {
-        return "gitHub1";
-    }
-}
+//package com.example.demo;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class YuliaControler {
+//    @RequestMapping("/gitHub")
+//    public String gitHub() {
+//
+//        return "gitHub";
+//    }
+//
+//    @RequestMapping("/programDesign")
+//    public String programDesign() {
+//        return "programDesign";
+//    }
+//
+//    @RequestMapping("/coreJava")
+//    public String coreJava() {
+//        return "coreJava";
+//    }
+//
+//    @RequestMapping("/oop")
+//    public String oop() {
+//        return "oop";
+//    }
+//
+//    @RequestMapping("/hcb")
+//    public String hcb() {
+//        return "hcb";
+//    }
+//
+//    @RequestMapping("/jbc")
+//    public String jbc() {
+//        return "jbc";
+//    }
+//
+//    @RequestMapping("gitHub1")
+//    public String gitHub1() {
+//        return "gitHub1";
+//    }
+//}
